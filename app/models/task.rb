4d7d273a59be83077e08bb82c08091b0db40823f
root <root@shehta>
2023-06-27 23:44:32 +0300

    more UI modifications

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
-  enum progress: [:to_do, :in_progress, :done], default: :to_do
+  # enum progress: [:to_do, :in_progress, :done]
+
+  validates :progress, presence: true
 
   belongs_to :task_pool
   has_and_belongs_to_many :users
